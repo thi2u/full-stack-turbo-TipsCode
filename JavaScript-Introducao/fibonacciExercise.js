d0ea35613d91af7484baaaa8139d70b21047eb01
Thiago Pinto <thiago.jsp02@gmail.com>
2026-01-03 23:04:24 -0300

    Finalizacao do modulo de JavaScript
    Segue atualizacao
    Nele fiz uma mudanca, implementando a ideia do colega de turma que usou if-else para acresentar os 2 primeiro numeros do array

--- JavaScript-Introducao/fibonacciExercise.js
+++ JavaScript-Introducao/fibonacciExercise.js
@@ -2,20 +2,27 @@
 const input = require('readline-sync');
 
 // Definindo Array
-const conjuntoFibonacci = [0, 1]
+// const conjuntoFibonacci = [0, 1]
+const conjuntoFibonacci = []
+
 
 // Input do usuário com total desejado
-let totalItens = input.question("Deseja qual tamanho para a sequencia?");
+let totalItens = input.question("Deseja qual tamanho para a sequencia? ");
 
 for(let numeroFibonacci; conjuntoFibonacci.length < totalItens;){
-    numeroFibonacci = somaNumeros(conjuntoFibonacci)
-    conjuntoFibonacci.push(numeroFibonacci)
+    if(conjuntoFibonacci.length < 1) {
+        conjuntoFibonacci.push(0)
+    } else if (conjuntoFibonacci.length < 2){
+        conjuntoFibonacci.push(1)
+    } else{
+        numeroFibonacci = somaNumeros(conjuntoFibonacci)
+        conjuntoFibonacci.push(numeroFibonacci)
+    }
 }
 
 console.log(conjuntoFibonacci.slice(0, totalItens))
 
-// Função que calcula os 2 últimos números no array
+// Função que calcula os 2 últimos elementos do array no array
 function somaNumeros(conjuntoFibonacci){
-
     return conjuntoFibonacci[conjuntoFibonacci.length - 1] + conjuntoFibonacci[conjuntoFibonacci.length - 2]
 }
