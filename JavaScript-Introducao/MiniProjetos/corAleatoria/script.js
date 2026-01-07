e8911e0355d793375695f116d57bc7d1dfb25c4a
Thiago Pinto <thiago.jsp02@gmail.com>
2026-01-07 17:16:46 -0300

    Finalizado projeto Cor Aleatório

--- JavaScript-Introducao/MiniProjetos/corAleatoria/script.js
+++ JavaScript-Introducao/MiniProjetos/corAleatoria/script.js
@@ -2,23 +2,19 @@ function mudaCor(){
     const colorR = Math.floor(Math.random()*256)
     const colorG = Math.floor(Math.random()*256)
     const colorB = Math.floor(Math.random()*256)
-    
-    
-    
-    printColor(colorR, colorB, colorG)
 
+    printColor(colorR, colorB, colorG)
 }
 
 // Funções para converter Cor RGB para Cor HEX
 
 function convertHex(numero){
-    
     let numeroConvertHex1 = Math.floor(numero / 16)
     let numeroConvertHex2 = Math.floor(numero % 16)
 
     numeroConvertHex1 = ajusteHex(numeroConvertHex1)
     numeroConvertHex2 = ajusteHex(numeroConvertHex2)
-    
+
     return `${numeroConvertHex1}${numeroConvertHex2}`
 }
 
