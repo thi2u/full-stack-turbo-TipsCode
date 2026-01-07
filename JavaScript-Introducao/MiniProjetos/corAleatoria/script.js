948ae2da835eca52e4bbd680e4521db0d369fc0a
Thiago Pinto <thiago.jsp02@gmail.com>
2026-01-07 15:55:45 -0300

    Finalizado projeto Cor Aleatório

--- JavaScript-Introducao/MiniProjetos/corAleatoria/script.js
+++ JavaScript-Introducao/MiniProjetos/corAleatoria/script.js
@@ -1,8 +1,51 @@
-const tableColor = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-]
+function mudaCor(){
+    const colorR = Math.floor(Math.random()*256)
+    const colorG = Math.floor(Math.random()*256)
+    const colorB = Math.floor(Math.random()*256)
+    
+    
+    
+    printColor(colorR, colorB, colorG)
+
+}
+
+// Funções para converter Cor RGB para Cor HEX
+
+function convertHex(numero){
+    
+    let numeroConvertHex1 = Math.floor(numero / 16)
+    let numeroConvertHex2 = Math.floor(numero % 16)
+
+    numeroConvertHex1 = ajusteHex(numeroConvertHex1)
+    numeroConvertHex2 = ajusteHex(numeroConvertHex2)
+    
+    //return String(numeroConvertHex1) + String(numeroConvertHex2)
+    return `${numeroConvertHex1}${numeroConvertHex2}`
+}
+
+function ajusteHex(numeroConvertHex){
+    const tableColor = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F"
+    ]
+    
+    if (numeroConvertHex < 10) {
+        return numeroConvertHex
+    } else {
+        return tableColor[numeroConvertHex-10]
+    }
+}
+
+function printColor(colorR, colorG, colorB){
+    const colorRGB = `(${colorR}, ${colorG}, ${colorB})`
+    const colorHex = "#" + convertHex(colorR) + convertHex(colorG) + convertHex(colorB)
+
+    document.querySelector("#rgb p").textContent = colorRGB
+    document.querySelector("#hex p").textContent = colorHex
+    
+    document.querySelector("#bodyVisible").style.backgroundColor = colorHex
+}
