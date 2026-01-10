13f25362614d2cb70f51ffeb0b5f2c0f817c7afe
Thiago Pinto <thiago.jsp02@gmail.com>
2026-01-10 09:27:59 -0300

    Finalizado desafio da Bateria Web
    Foram adicionados codigos para captura do clique do mouse e teclas pressionadas.
    Com a captura do input fazemos um cruzamento de referencias entre arrays e retornamos o som da nossa bateria

--- JavaScript-Introducao/projeto-bateria-start/app.js
+++ JavaScript-Introducao/projeto-bateria-start/app.js
@@ -1,4 +1,6 @@
 const botoes = document.querySelectorAll(".set button")
+const bodySelected = document.querySelector("body")
+
 const fileSounds = [
     './sounds/crash.mp3',
     './sounds/kick-bass.mp3',
@@ -9,12 +11,49 @@ const fileSounds = [
     './sounds/tom-4.mp3'
 ]
 
-    botoes.forEach(handleClick)
+const keysPressed = [
+    "KeyW",
+    "KeyA",
+    "KeyS",
+    "KeyD",
+    "KeyJ",
+    "KeyK",
+    "KeyL"
+]
 
-    function handleClick(item, indice){
-       item.addEventListener("click", function (){
-
-        console.log("Você clicou no botão " + item.innerHTML + ", na posição " + indice)
-       }
-        )
+// Função que verifica se o usuário clicou em um dos elementos de nossa tela
+keysPressed.forEach(keyClick)
+function keyClick(item, indice){
+    bodySelected.addEventListener("keydown", function (e){
+        if (e.code == item){
+        tocarAudio(indice)
+        }
+        console.log (e.code)
     }
+)
+}
+botoes.forEach(handleClick)
+function handleClick(item, indice){
+    item.addEventListener("click", function (){
+        tocarAudio(indice)
+    }
+)
+}
+
+// Função que captura as teclas pressionadas e verifica se é igual às teclas indicadas em nosso array, que identifica W, A, S, D, J, K, L
+keysPressed.forEach(keyClick)
+function keyClick(item, indice){
+    bodySelected.addEventListener("keydown", function (e){
+        if (e.code == item){
+        tocarAudio(indice)
+        }
+        console.log (e.code)
+    }
+)
+}
+
+// Função para tocar o áudio
+function tocarAudio(indice){    
+    const audioDrum = new Audio(fileSounds[indice])
+    audioDrum.play()
+}
